6728d44b9129ec9cf1c182b023849c7c04e25422
Letícia Videira Gois <leticia.videira.gois@gmail.com>
2025-10-28 15:02:41 -0300

    Criação file para structs

---
+++ src/struct.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "formas.h"
+#include "pilha.h"
+
+typedef struct{
+    formaTipo tipoF;
+    void* data;
+} FormaSt;
+
+typedef struct{
+    int id;
+    PILHA *formas;
+}carregadorSt;
+
+typedef struct {
+    int id;
+    double x, y;
+    carregadorSt *cargaEsquerda;
+    carregadorSt *cargaDireita;
+    FormaSt *emDisparo;
+    int cargaEsqId;
+    int cargaDirId;
+} disparadorSt;
+
+typedef struct{
+    FormaSt *forma;
+    double x, y;
+    int iA; //bool
+    double disparadorX;
+    double disparadorY;
+} PosicaoFormaASt;
+
+typedef enum {
+    ARRAYDISPARADORF, ARRAYCARREGADORF, POSICAOFORMAF, PILHAF
+} TIPOF;
+
+typedef struct{
+    void *p;
+    TIPOF tp;
+} ItemFree;
+
+
+typedef struct {
+    double minX, minY;
+    double maxX, maxY;
+} bBox;
+
