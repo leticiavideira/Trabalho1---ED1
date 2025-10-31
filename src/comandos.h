9877e5d7cd03c03eb7835de3b466117f319c8c8a
Letícia Videira Gois <leticia.videira.gois@gmail.com>
2025-10-30 23:45:02 -0300

    Novos parâmetros nas funções

--- src/comandos.h
+++ src/comandos.h
@@ -6,19 +6,19 @@
 #include "leitorGeo.h"
 #include "auxiliares.h"
 
-void pdExecutar (disparadorSt **disparador, int *contDisparos, PILHA pilhaFree);
+void pdExecutar (disparadorSt **disparador, int *contDisparos, PILHA pilhaFree, Estatisticas *stats);
 
-void lcExecutar (carregadorSt **carregador, int *contCarregador, CHAO chao, PILHA pilhaFree, FILE *txtFile);
+void lcExecutar (carregadorSt **carregador, int *contCarregador, CHAO chao, PILHA pilhaFree, FILE *txtFile, Estatisticas *stats);
 
-void atchExecutar (carregadorSt **carregador, int *contCarregador, disparadorSt **disparador, int *contDisparos, PILHA pilhaFree);
+void atchExecutar (carregadorSt **carregador, int *contCarregador, disparadorSt **disparador, int *contDisparos, PILHA pilhaFree, Estatisticas *stats);
 
-void shftExecutar (disparadorSt **disparador, int *contDisparos, carregadorSt *carregador, int *contCarregador, FILE *txtFile);
+void shftExecutar (disparadorSt **disparador, int *contDisparos, carregadorSt *carregador, int *contCarregador, FILE *txtFile, Estatisticas *stats);
 
-void dspExecutar (disparadorSt **disparador, int *contDisparos, PILHA arena, PILHA pilhaFree, FILE *txtFile);
+void dspExecutar (disparadorSt **disparador, int *contDisparos, PILHA arena, PILHA pilhaFree, FILE *txtFile, Estatisticas *stats);
 
-void rjdExecutar (disparadorSt **disparador, int *contDisparos, PILHA pilhaFree, PILHA arena, carregadorSt *carregador, int *contCarregador, FILE *txtFile);
+void rjdExecutar (disparadorSt **disparador, int *contDisparos, PILHA pilhaFree, PILHA arena, carregadorSt *carregador, int *contCarregador, FILE *txtFile, Estatisticas *stats);
 
-void calcExecutar (PILHA arena, CHAO chao, FILE *txtFile);
+void calcExecutar (PILHA arena, CHAO chao, FILE *txtFile, Estatisticas *stats);
 
 
 #endif
